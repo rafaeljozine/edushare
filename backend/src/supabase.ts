e1bd25aa10a80c9ab6606878cdd215ea0921e191
Rafael Jozine <rafaeljozine@gmail.com>
2026-06-08 04:55:03 +0200

    debug supabase

--- backend/src/supabase.ts
+++ backend/src/supabase.ts
@@ -4,3 +4,4 @@ export const supabase = createClient(
   process.env.SUPABASE_URL as string,
   process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
+console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
