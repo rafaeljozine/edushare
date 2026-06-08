adcb53de995b483d40680cdbcbedf3eadfe4c538
Rafael Jozine <rafaeljozine@gmail.com>
2026-06-08 05:12:21 +0200

    supa

--- backend/src/supabase.ts
+++ backend/src/supabase.ts
@@ -1,7 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("URL:", process.env.SUPABASE_URL);
+console.log(
+  "KEY:",
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+    ? "EXISTE"
+    : "VAZIA"
+);
+
 export const supabase = createClient(
   process.env.SUPABASE_URL as string,
   process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
-console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
